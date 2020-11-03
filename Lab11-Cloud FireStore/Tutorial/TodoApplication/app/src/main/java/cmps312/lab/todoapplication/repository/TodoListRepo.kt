748984472178c77f0a064295fc64ffc4456dc516
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-11-03 14:47:42 +0300

    update

--- Lab11-Cloud FireStore/Tutorial/TodoApplication/app/src/main/java/cmps312/lab/todoapplication/repository/TodoListRepo.kt
+++ Lab11-Cloud FireStore/Tutorial/TodoApplication/app/src/main/java/cmps312/lab/todoapplication/repository/TodoListRepo.kt
@@ -13,24 +13,41 @@ object TodoListRepo {
         FirebaseFirestore.getInstance()
     }
 
-    val projectDocumentRef  by lazy { db.collection("projects") }
+    val projectDocumentRef by lazy { db.collection("projects") }
     val todosDocumentRef by lazy { db.collection("todos") }
 
     init {
         db.firestoreSettings = firestoreSettings { isPersistenceEnabled = true }
-     }
+    }
 
-    suspend fun getProjects()= projectDocumentRef.get().await().toObjects(Project::class.java)
+//    suspend fun getProjects()= projectDocumentRef.get().await().toObjects(Project::class.java)
 
     fun addProject(project: Project) = projectDocumentRef.add(project)
         .addOnSuccessListener { Log.d(TAG, "Project successfully added: ") }
-        .addOnFailureListener { Log.d(TAG, "Failed to add Project ")}
+        .addOnFailureListener { Log.d(TAG, "Failed to add Project ") }
 
 
-    suspend fun deleteProject(project: Project) = null
-    suspend fun getTodoListByProject(pid: String) = null
-    suspend fun addTodo(todo: Todo) = null
-    suspend fun getTodo(id: String) =null
-    suspend fun deleteTodo(todo: Todo) = null
-    suspend fun updateToDo(todo: Todo) = null
+    suspend fun deleteProject(project: Project) =
+        projectDocumentRef.document(project.projectId).delete().await()
+
+    suspend fun getTodoListByProject(pid: String): MutableList<Todo> {
+        val snapShot = todosDocumentRef.whereEqualTo("projectId", pid).get().await()
+        val todos = mutableListOf<Todo>()
+        snapShot.forEach {
+            val todo = it.toObject(Todo::class.java)
+            todo.todoId = it.id
+            todos.add(todo)
+        }
+        return todos
+    }
+
+    fun addTodo(todo: Todo) = todosDocumentRef.add(todo)
+        .addOnSuccessListener { Log.d(TAG, "Todo successfully added: ") }
+        .addOnFailureListener { Log.d(TAG, "Failed to add Todo ") }
+
+    suspend fun getTodo(id: String) =
+        todosDocumentRef.document(id).get().await().toObject(Todo::class.java)
+
+    fun deleteTodo(id: String) = todosDocumentRef.document(id).delete()
+    fun updateToDo(todo: Todo) = todo.todoId?.let { todosDocumentRef.document(it).set(todo) }
 }
--- Lab11-Cloud FireStore/Tutorial/TodoApplication/app/src/main/java/cmps312/lab/todoapplication/ui/viewmodel/ProjectViewModel.kt
+++ Lab11-Cloud FireStore/Tutorial/TodoApplication/app/src/main/java/cmps312/lab/todoapplication/ui/viewmodel/ProjectViewModel.kt
@@ -10,6 +10,7 @@ import cmps312.lab.todoapplication.model.Todo
 import cmps312.lab.todoapplication.repository.TodoListRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class ProjectViewModel(application: Application) : AndroidViewModel(application) {
@@ -20,30 +21,32 @@ class ProjectViewModel(application: Application) : AndroidViewModel(application)
     var projects: LiveData<List<Project>> = _projects
     var todos: LiveData<List<Todo>> = _todos
 
-    lateinit var selectedTodo: Todo
+    var selectedTodo: Todo? = null
     var selectedProject: Project? = null
 
-
+    init {
+        listenToProjectsCollection()
+        listenToTodosCollection()
+    }
 
     fun getTodos(projectId: String) {
         _todos.value = listOf<Todo>() //clear the list
         viewModelScope.launch(Dispatchers.IO) {
-            withContext(Dispatchers.Main){
-//                _todos.value =
-//                    cmps312.lab.todoapplication.repository.TodoListRepo.getTodoListByProject(projectId)
+            withContext(Dispatchers.Main) {
+                _todos.value = TodoListRepo.getTodoListByProject(projectId)
             }
         }
     }
 
     fun addTodo(todo: Todo) {
         viewModelScope.launch(Dispatchers.IO) {
-//            TodoListRepo.addTodo(todo).await()
+            TodoListRepo.addTodo(todo).await()
         }
     }
 
     fun deleteTodo(todo: Todo) {
         viewModelScope.launch(Dispatchers.IO) {
-            TodoListRepo.deleteTodo(todo)
+            todo.todoId?.let { TodoListRepo.deleteTodo(it) }
         }
     }
 
@@ -55,13 +58,46 @@ class ProjectViewModel(application: Application) : AndroidViewModel(application)
 
     fun addProject(project: Project) {
         viewModelScope.launch(Dispatchers.IO) {
-            TodoListRepo.addProject(project)
+            TodoListRepo.addProject(project).await()
         }
     }
 
     fun deleteProject(project: Project) {
         viewModelScope.launch(Dispatchers.IO) {
             TodoListRepo.deleteProject(project)
+        }
+    }
+
+    //listen for new project
+    private fun listenToProjectsCollection() {
+        TodoListRepo.projectDocumentRef.addSnapshotListener { snapshot, error ->
+            if (error != null) return@addSnapshotListener
+//            _projects.value = snapshot!!.toObjects(Project::class.java)
+
+            val updatedProject = mutableListOf<Project>()
+            snapshot!!.forEach {
+                val project = it.toObject(Project::class.java)
+                project.projectId = it.id
+                updatedProject.add(project)
+            }
+            _projects.value = updatedProject
+        }
+    }
+
+    private fun listenToTodosCollection() {
+        TodoListRepo.todosDocumentRef.addSnapshotListener { snapshot, error ->
+            if (error != null) return@addSnapshotListener
+
+            val updatedTodo = mutableListOf<Todo>()
+            if (selectedProject != null) {
+                snapshot!!.forEach {
+                    val todo = it.toObject(Todo::class.java)
+                    todo.todoId = it.id
+                    if (selectedProject!!.projectId == todo.projectId)
+                        updatedTodo.add(todo)
+                }
+                _todos.value = updatedTodo
+            }
         }
     }
 
