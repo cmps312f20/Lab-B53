4d5705f68c18ae544c9dadfc5df6443272e1c1eb
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-11-10 13:54:57 +0300

    updated 1:54

--- Lab 12-CloudFireStore Storage Auth with WorkManager /app/src/main/java/cmps312/lab/lab12storagewithworkmanager/MainActivity.kt
+++ Lab 12-CloudFireStore Storage Auth with WorkManager /app/src/main/java/cmps312/lab/lab12storagewithworkmanager/MainActivity.kt
@@ -2,10 +2,7 @@ package cmps312.lab.lab12storagewithworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import androidx.work.WorkRequest
+import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
@@ -15,8 +12,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
-        request = OneTimeWorkRequestBuilder<MyWorker>().build()
+
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresBatteryNotLow(true)
+                .build()
+
+        request = OneTimeWorkRequestBuilder<MyWorker>()
+                .setConstraints(constraints)
+                .build()
+
+        WorkManager.getInstance(this)
+                .getWorkInfoByIdLiveData(request.id)
+                .observe(this){
+                    workStatusTv.append("${it.state.name}\n")
+                }
 //        request = PeriodicWorkRequestBuilder<MyWorker>(1, TimeUnit.MINUTES).build()
 
         startWorkBtn.setOnClickListener {
--- Lab 12-CloudFireStore Storage Auth with WorkManager /app/src/main/java/cmps312/lab/lab12storagewithworkmanager/MyWorker.kt
+++ Lab 12-CloudFireStore Storage Auth with WorkManager /app/src/main/java/cmps312/lab/lab12storagewithworkmanager/MyWorker.kt
@@ -1,18 +1,53 @@
 package cmps312.lab.lab12storagewithworkmanager
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.work.CoroutineWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 
-class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
-    override fun doWork(): Result {
-        showNotification("Completed the work")
-        return Result.success()
+class MyWorker(context: Context, workerParams: WorkerParameters)
+    : CoroutineWorker(context, workerParams) {
+
+    override suspend fun doWork(): Result = coroutineScope{
+        val jobs = (0..10).map {
+            async { downloadSomethingBig(it) }
+        }
+//        jobs.awaitAll()
+
+        Result.success()
     }
 
+    private suspend fun downloadSomethingBig(num : Int){
+        delay(1000)
+        showNotification("Completed the background work $num")
+    }
     private fun showNotification(completed : String){
-        Log.d("TAG", "showNotification: ")
+        val manager : NotificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel("Woker1", "Worker Manager",
+                NotificationManager.IMPORTANCE_DEFAULT)
+
+            val notification = NotificationCompat.Builder(applicationContext, "Woker1")
+                .setContentTitle("Work Manager Notification")
+                .setContentText(completed)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
+
+            manager.createNotificationChannel(channel)
+            manager.notify(1, notification)
+        }
+
     }
 }
